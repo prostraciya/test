3b32ba9832eda0bc47f1483cb10ad0888c76f9af
denisko.ds <denisko.ds@gmail.com>
2018-05-04 10:19:06 +0300

    test2

--- slices1.py
+++ slices1.py
@@ -1,2 +1,7 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3])
+
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+for player in players[:]:
+    print(player.title())
